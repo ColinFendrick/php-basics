0885f9cc3fe312a583aacbfa1c548fe41d97634a
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-12 17:51:05 -0400

    for loops

--- arrays.php
+++ arrays.php
@@ -10,6 +10,10 @@
       $lunch[2] = "Beans? Fuck beans we're having gouda cheese on this.";
       echo $lunch[2];
 
+      //Modifying and deleting arrays
+      unset($lunch[1]);
+      unset($lunch);
+
     ?>    
   </body>
 </html>
