3e98b6445359495fb31cc2882d72681a45680c30
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-12 19:17:52 -0400

    Objects basics

---
+++ objects.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Reconstructing the Person Class</title>
+	</head>
+	<body>
+      <p>
+        <?php
+            class Person {}
+            $teacher = new Person();
+            $student = new Person();
+        ?>
+      </p>
+    </body>
+</html>
