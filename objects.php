41b88f30d9a6777c0a7b284df59741e9e55adf96
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-12 19:23:05 -0400

    constructor

--- objects.php
+++ objects.php
@@ -6,9 +6,21 @@
 	<body>
       <p>
         <?php
-            class Person {}
+            class Person {
+             public $isAlive = true;
+             public $firstname;
+             public $lastname;
+             public $age;
+
+             public function __construct($firstname, $lastname, $age) {
+               $this->firstname = $firstname;
+               $this->lastname = $lastname;
+               $this->age = $age;
+             }
+            }
             $teacher = new Person();
             $student = new Person();
+            echo $teacher->isAlive;
         ?>
       </p>
     </body>
