1a8f07114102b18ea5b6d6e5730f6adc91b8dd0f
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-13 10:21:01 -0400

    associative arrays

---
+++ advanced-arrays.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Making the Connection</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        // This is an array using integers as the indices.
+        // Add 'BMW' as the last element in the array!
+        $car = array(2012, 'blue', 5);
+
+        // This is an associative array.
+        // Add the make => 'BMW' key/value pair!
+        $assocCar = array('year' => 2012,
+                   'colour' => 'blue',
+                   'doors' => 5);
+            
+        // This code should output "BMW"...
+        echo $car[3];
+        echo '<br />';
+            
+        // ...and so should this!
+        echo $assocCar['make'];
+      ?>
+    </p>
+  </body>
+</html>
